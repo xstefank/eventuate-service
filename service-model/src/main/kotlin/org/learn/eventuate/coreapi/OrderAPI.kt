9ad6b5ef45f502479e96751c9d8ce1ea32cb0e0c
xstefank <xstefank122@gmail.com>
2017-10-15 20:51:18 +0200

    add service-model project

---
+++ service-model/src/main/kotlin/org/learn/eventuate/coreapi/OrderAPI.kt
@@ -0,0 +1,30 @@
+package org.learn.eventuate.coreapi
+
+
+//model
+data class ProductInfo(val productId: String = "", val comment: String = "", val price: Int = 0)
+
+
+class FileOrderCommand(val orderId: String, val productInfo : ProductInfo)
+class OrderFiledEvent(val orderId: String, val productInfo : ProductInfo)
+class OrderCompletedEvent(val orderId: String, val productInfo: ProductInfo)
+
+
+class PrepareShipmentCommand(val orderId: String, val productInfo: ProductInfo)
+class ShipmentPreparedEvent(val shipmentId: String, val orderId: String, val price: Int)
+class ShipmentPreparationFailedEvent(val shipmentId: String, val orderId: String, val cause: String)
+
+
+class PrepareInvoiceCommand(val orderId: String, val productInfo: ProductInfo)
+class InvoicePreparedEvent(val invoiceId: String, val orderId: String, val invoice: String)
+class InvoicePreparationFailedEvent(val invoiceId: String, val orderId: String, val cause: String)
+
+
+//compensation
+
+class CompensateShipmentCommand(val orderId: String, val cause: String)
+class ShipmentCompensatedEvent(val shipmentId: String, val orderId: String, val description: String)
+
+
+class CompensateInvoiceCommand(val orderId: String, val cause: String)
+class InvoiceCompensatedEvent(val invoiceId: String ,val orderId: String, val description: String)
