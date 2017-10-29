58a7b08ea3eca791d958eaff257313c3a685c53c
xstefank <xstefank122@gmail.com>
2017-10-29 09:38:00 +0100

    add invoice-service module

---
+++ invoice-service/src/main/java/org/learn/eventuate/invservice/InvServiceApplication.java
@@ -0,0 +1,12 @@
+package org.learn.eventuate.invservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvServiceApplication.class, args);
+	}
+}
