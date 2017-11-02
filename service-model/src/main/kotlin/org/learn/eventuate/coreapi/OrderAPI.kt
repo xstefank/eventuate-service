5cc483a80fb01599891d99c8f18669f766b8ae30
xstefank <xstefank122@gmail.com>
2017-11-02 23:01:29 +0100

    clean up service-model

--- service-model/src/main/kotlin/org/learn/eventuate/coreapi/OrderAPI.kt
+++ service-model/src/main/kotlin/org/learn/eventuate/coreapi/OrderAPI.kt
@@ -14,23 +14,3 @@ data class InvoiceInfo(val sagaId: String = "", val invoice: String = "")
 
 data class OrderFiledEvent(val orderId: String = "", val productInfo: ProductInfo? = null) : OrderEvent
 class OrderCompletedEvent(val orderId: String, val productInfo: ProductInfo) : OrderEvent
-
-
-class PrepareShipmentCommand(val orderId: String, val productInfo: ProductInfo)
-class ShipmentPreparedEvent(val shipmentId: String, val orderId: String, val price: Int)
-class ShipmentPreparationFailedEvent(val shipmentId: String, val orderId: String, val cause: String)
-
-
-class PrepareInvoiceCommand(val orderId: String, val productInfo: ProductInfo)
-class InvoicePreparedEvent(val invoiceId: String, val orderId: String, val invoice: String)
-class InvoicePreparationFailedEvent(val invoiceId: String, val orderId: String, val cause: String)
-
-
-//compensation
-
-class CompensateShipmentCommand(val orderId: String, val cause: String)
-class ShipmentCompensatedEvent(val shipmentId: String, val orderId: String, val description: String)
-
-
-class CompensateInvoiceCommand(val orderId: String, val cause: String)
-class InvoiceCompensatedEvent(val invoiceId: String ,val orderId: String, val description: String)
