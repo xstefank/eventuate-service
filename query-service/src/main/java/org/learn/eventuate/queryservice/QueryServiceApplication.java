3e8dd46e59be28138151270e6e87d8756eb5b646
xstefank <xstefank122@gmail.com>
2017-11-10 21:41:14 +0100

    add query-service

---
+++ query-service/src/main/java/org/learn/eventuate/queryservice/QueryServiceApplication.java
@@ -0,0 +1,12 @@
+package org.learn.eventuate.queryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueryServiceApplication.class, args);
+	}
+}
